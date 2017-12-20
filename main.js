376a3f6ec658c3eb2f450a2582917195dbb2c6dd
Viktor Tatarnikov <viktor@tatarnikov.org>
2017-12-20 12:15:53 +0600

    Правка строки запуска билдера для MacOS X, и исправление бага copy\paste в electron на Mac OS X

--- main.js
+++ main.js
@@ -1,12 +1,76 @@
 const electron = require('electron')
-const app = electron.app
+const {app, Menu, shell} = require('electron')
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
-const shell = require('electron').shell
 
 let mainWindow
+
+function createMenu() {
+	var menu_template = [{
+		label: app.getName(),
+		submenu: [
+	      {role: 'about'},
+	      {type: 'separator'},
+	      {role: 'services', submenu: []},
+	      {type: 'separator'},
+	      {role: 'hide'},
+	      {role: 'hideothers'},
+	      {role: 'unhide'},
+	      {type: 'separator'},
+	      {role: 'quit'}
+    ]},
+	{
+	    label: 'Edit',
+	    submenu: [
+	      {
+	        label: 'Cut',
+	        accelerator: 'Command+X',
+	        selector: 'cut:'
+	      },
+	      {
+	        label: 'Copy',
+	        accelerator: 'Command+C',
+	        selector: 'copy:'
+	      },
+	      {
+	        label: 'Paste',
+	        accelerator: 'Command+V',
+	        selector: 'paste:'
+	      },
+	      {
+	        label: 'Select All',
+	        accelerator: 'Command+A',
+	        selector: 'selectAll:'
+	      },
+	    ]
+	  },
+	  {
+	    label: 'Window',
+	    submenu: [
+	      {
+	        label: 'Minimize',
+	        accelerator: 'Command+M',
+	        selector: 'performMiniaturize:'
+	      },
+	      {
+	        label: 'Close',
+	        accelerator: 'Command+W',
+	        selector: 'performClose:'
+	      },
+	      {
+	        type: 'separator'
+	      },
+	      {
+	        label: 'Bring All to Front',
+	        selector: 'arrangeInFront:'
+	      },
+	    ]
+	}];
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(menu_template));
+}
 
 function createWindow () {
   mainWindow = new BrowserWindow({minWidth: 800, minHeight: 600, frame: false, resizable: true, fullscreenable: true})
@@ -23,6 +87,7 @@ function createWindow () {
 }
 
 app.on('ready', function () {
+	createMenu()
 	createWindow()
 })
 
